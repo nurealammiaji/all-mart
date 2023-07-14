3f1a282319ccf9f9ca3479f0d07cf7b6b3ec879e
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-14 22:18:54 +0600

    nav bar style modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.svg'
 const Header = () => {
     return (
         <nav className='nav-bar'>
-            <div>
+            <div className='nav-logo'>
                 <img src={logo} alt="" />
             </div>
             <div className='nav-link'>
