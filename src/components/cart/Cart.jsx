602def84ad7a2c96135d4de8d1793f688695b6e8
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-15 23:22:53 +0600

    cart calculation added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,14 +1,27 @@
 import './Cart.css'
 
 const Cart = (props) => {
+
+    console.log(props.items);
+
+    const items = props.items;
+
+    let price = 0;
+    let shipping = 0;
+    for (const item of items) {
+        console.log(item.price);
+        price = price + item.price;
+        shipping = shipping + item.shipping;
+    }
+
     return (
         <div>
                 <div className='cart-container'>
                     <h4>Order Summary</h4>
                     <div className='cart-info'>
-                        <p>Selected Items: {props.items.length} </p>
-                        <p>Total Price: </p>
-                        <p>Total Shipping Charge: </p>
+                        <p>Selected Items: {items.length}</p>
+                        <p>Total Price: ${price}</p>
+                        <p>Total Shipping Charge: ${shipping}</p>
                         <p>Tax: </p>
                         <h5>Grand Total: </h5>
                     </div>
